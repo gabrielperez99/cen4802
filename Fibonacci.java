21a202897d4bdaddd4243ed08a6be7c6f275e876
gabrielperez99 <113302988+gabrielperez99@users.noreply.github.com>
2023-05-21 19:54:06 -0400

    Add files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,33 @@
+package cen4802;
+
+import java.util.Scanner;
+
+public class Fibonacci {
+	  
+	    public static int fibonacci(int n) {
+	        if (n <= 1) {
+	            return n;
+	        }
+	        
+	        // Calculate the nth term by using the previous terms
+	        return fibonacci(n - 1) + fibonacci(n - 2);
+	    }
+
+	    // Main method for Fibonacci sequence
+	    public static void main(String[] args) {
+	        // Create a Scanner object 
+	        Scanner scanner = new Scanner(System.in);
+	        // Enter N value
+	        System.out.print("Enter the value of n: ");
+	        int n = scanner.nextInt();
+
+	        // Close the Scanner 
+	        scanner.close();
+
+	        // Calculate the nth term 
+	        int nthTerm = fibonacci(n);
+
+	        // Output
+	        System.out.println("The " + n + "th term of the Fibonacci sequence is " + nthTerm + ".");
+	    }
+	}
